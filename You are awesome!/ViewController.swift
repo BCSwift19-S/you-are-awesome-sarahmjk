dd2681fe9cd3d37ebfd015f212058bd428dcf5d7
Sarah Kim <sarah.mjk@hotmail.com>
2019-01-23 03:53:18 -0500

    2.9 complete

--- You are awesome!/ViewController.swift
+++ You are awesome!/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
 
     @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
     
     //code below executes when the app's view first loads
     override func viewDidLoad() {
@@ -21,20 +23,40 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-
-        let message1 = "You are fantastic!!!"
-        let message2 = "You are great!"
-        let message3 = "You are amazing!"
-        
-        if messageLabel.text == message1 {
-            messageLabel.text = message2
+        let messages = ["You are awsome!",
+                        "You are great!",
+                        "You are fantastic!",
+                        "When the Genius Bar needs help, they call you",
+                        "You brighten my day!",
+                        "You are the bomb!",
+                        "Hey, fabulous!",
+                        "You are tremendous!",
+                        "You've got the design skills of Johnny Ive!",
+                        "I can't wait to download your code!"]
+        messageLabel.text = messages[0]
+        if index == messages.count-1 {
+            index=0
+        }else {index=index+1
             
-        } else if messageLabel.text == message2 {
-            messageLabel.text = message3
-        }else {
-            messageLabel.text = message1
         }
         
+        
+        
+
+        
+//        let message1 = "You are fantastic!!!"
+//        let message2 = "You are great!"
+//        let message3 = "You are amazing!"
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        }else {
+//            messageLabel.text = message1
+//        }
+//
         
     }
    
