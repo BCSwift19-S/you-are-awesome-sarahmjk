560208f21793e4bf9fe5faa6836f5d4152bb55f8
Sarah Kim <sarah.mjk@hotmail.com>
2019-01-22 20:52:00 -0500

    2.3 completed

--- You are awesome!/ViewController.swift
+++ You are awesome!/ViewController.swift
@@ -4,17 +4,25 @@
 //
 //  Created by Sarah Minji Kim on 1/13/19.
 //  Copyright © 2019 Sarah Minji Kim. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    //code below executes when the app's view first loads
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-//
 
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You are awesome!"
+        
+    }
+    
 }
 
